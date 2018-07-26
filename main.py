48404227ab029eddff07c3a3bcd89419201b12a0
Ray <rayliu109@gmail.com>
2018-07-25 20:47:44 -0400

    Added UI elements

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import threading
 from receptorInput import *
 from receptorInput import popupmsg as popupmsg
 from sourceEditor  import *
+import time
+from PIL import ImageTk, Image
 
 
 
@@ -63,14 +65,32 @@ def runAERMET():
 
 
 
+class Splash(tk.Toplevel):
+    def __init__(self, parent):
+        tk.Toplevel.__init__(self, parent)
+        self.title("ARTISC Version 1.8.1")
+
+        self.geometry("400x358")
+        path  = "C:\\Users\\Ray\\Desktop\\EPA-Interface\\EPA-Interface\\Pictures\\companylogy.jpg"
+        img = ImageTk.PhotoImage(Image.open(path))
+
+        imgicon = ImageTk.PhotoImage(file=os.path.join('C:\\Users\\Ray\\Desktop\\EPA-Interface\\EPA-Interface\\', 'company.ico'))
+        self.tk.call('wm', 'iconphoto', self._w, imgicon)
+        panel = tk.Label(self, image=img)
+        panel.pack(side="bottom", fill="both", expand="yes")
+        ## required to make window show before the program gets to the mainloop
+        self.update()
 
 class EPAgui(tk.Tk):
 
     def __init__(self, *arg, **kwargs):
         tk.Tk.__init__(self, *arg, **kwargs) #convention, *args -> any number of variables, **kwargs -> pass through dictionaries
 
-        tk.Tk.wm_title(self, "EPA Client")
 
+        tk.Tk.wm_title(self, "ARTISC Version 1.8.1")
+        imgicon = ImageTk.PhotoImage(file=os.path.join('C:\\Users\\Ray\\Desktop\\EPA-Interface\\EPA-Interface\\', 'company.ico'))
+
+        self.tk.call('wm', 'iconphoto', self._w, imgicon)
         container = tk.Frame(self)
         container.pack(side="top", fill="both", expand=True)
         container.grid_rowconfigure(0, weight=1) # 0 -> min size
@@ -78,83 +98,137 @@ class EPAgui(tk.Tk):
 
         menubar = tk.Menu(container)
 
-        filemenu = tk.Menu(menubar, tearoff = 0)
-        filemenu.add_command(label="New Session", command = lambda: popupmsg("Not supported yet!",""))
-        filemenu.add_command(label="Load File", command=lambda: popupmsg("Not supported yet!",""))
+        filemenu = tk.Menu(menubar, tearoff=0)
+        filemenu.add_command(label="New Session", command=lambda: threading.Thread(target=popupmsg("Not supported yet!", "")).start())
+        filemenu.add_command(label="Load File", command=lambda: threading.Thread(target=popupmsg("Not supported yet!", "")).start())
         filemenu.add_separator()
-        filemenu.add_command(label = "Exit", command = quit)
+        filemenu.add_command(label="Exit", command=quit)
         menubar.add_cascade(label="檔案 (File)"
                                   "", menu=filemenu)
 
         editmenu = tk.Menu(menubar, tearoff=0)
-        editmenu.add_command(label="Undo", command=lambda: popupmsg("Not supported yet!",""))
-        editmenu.add_command(label="Redo", command=lambda: popupmsg("Not supported yet!",""))
-        tk.Tk.config(self, menu= menubar)
-        menubar.add_cascade(label="Edit", menu=editmenu)
+        editmenu.add_command(label="汙染源", command=lambda: threading.Thread(target=sourceBox).start())
+        editmenu.add_command(label="接受體", command=lambda: threading.Thread(target=numRec).start())
+        editmenu.add_command(label="地理/地形", command=lambda: threading.Thread(target=popupmsg("Not supported yet!", "")).start())
+        editmenu.add_command(label="氣候", command=lambda: threading.Thread(target=popupmsg("Not supported yet!", "")).start())
+        editmenu.add_command(label="建築物", command=lambda: threading.Thread(target=popupmsg("Not supported yet!", "")).start())
+        # runmenu.add_command(label="Run AERSCREEN testcase", command=lambda: threading.Thread(target=runAERScreen).start())
+        menubar.add_cascade(label="汙染環境(Environment input)", menu=editmenu)
 
-        runmenu = tk.Menu(menubar, tearoff=0)
-        runmenu.add_command(label="Run AERMAP testcase", command=lambda: threading.Thread(target=runAERMAP).start())
-        runmenu.add_command(label="Run AERMOD testcase", command=lambda: threading.Thread(target=runAERMOD).start())
-        runmenu.add_command(label="Run AERMET testcase", command=lambda: threading.Thread(target=runAERMET).start())
-        runmenu.add_command(label="Edit AERMOD Source", command=lambda: threading.Thread(target=sourceBox).start())
-        runmenu.add_command(label="Edit AERMOD Receptor", command=lambda: threading.Thread(target=numRec).start())
-        runmenu.add_command(label="Run AERMOD with input", command=lambda: threading.Thread(target=receptorInput).start())
-        #runmenu.add_command(label="Run AERSCREEN testcase", command=lambda: threading.Thread(target=runAERScreen).start())
-        runmenu.add_command(label="Run AERSCREEN", command=lambda: threading.Thread(target=popinput).start())
+        sensormenu = tk.Menu(menubar, tearoff=0)
+        sensormenu.add_command(label="汙染源", command=lambda: threading.Thread(target=popupmsg("Not supported yet!", "")).start())
+        sensormenu.add_command(label="接受體", command=lambda: threading.Thread(target=popupmsg("Not supported yet!", "")).start())
+        sensormenu.add_command(label="地理/地形", command=lambda: threading.Thread(target=popupmsg("Not supported yet!", "")).start())
+        sensormenu.add_command(label="氣候", command=lambda:threading.Thread(target=popupmsg("Not supported yet!", "")).start())
+        sensormenu.add_command(label="建築物", command=lambda: threading.Thread(target=popupmsg("Not supported yet!", "")).start())
         tk.Tk.config(self, menu=menubar)
-        menubar.add_cascade(label="執行 (Run)", menu=runmenu)
+        menubar.add_cascade(label="汙染監控(Environment sensors)", menu=sensormenu)
+
+        simuMenu = tk.Menu(menubar, tearoff=0)
+        simuMenu.add_command(label="分鐘", command=lambda: threading.Thread(target=popupmsg("Not supported yet!", "")).start())
+        simuMenu.add_command(label="小時", command=lambda: threading.Thread(target=popupmsg("Not supported yet!", "")).start())
+        simuMenu.add_command(label="天日", command=lambda: threading.Thread(target=popupmsg("Not supported yet!", "")).start())
+        simuMenu.add_command(label="禮拜", command=lambda: threading.Thread(target=popupmsg("Not supported yet!", "")).start())
+        simuMenu.add_command(label="月", command=lambda: threading.Thread(target=popupmsg("Not supported yet!", "")).start())
+        simuMenu.add_command(label="年", command=lambda:threading.Thread(target=popupmsg("Not supported yet!", "")).start())
+        simuMenu.add_command(label="Run AERMOD with input", command= lambda: threading.Thread(target=receptorInput).start())
+
+        menubar.add_cascade(label="汙染模擬(Simulation)", menu=simuMenu)
+
+        resultMenu = tk.Menu(menubar, tearoff=0)
+        resultMenu.add_command(label="過程檔案", command=lambda: threading.Thread(target=popupmsg("Not supported yet!", "")).start())
+        resultMenu.add_command(label="解果檔案", command=lambda: threading.Thread(target=popupmsg("Not supported yet!", "")).start())
+        resultMenu.add_command(label="Debug file", command=lambda: threading.Thread(target=popupmsg("Not supported yet!", "")).start())
+        resultMenu.add_command(label="Plotfile", command=lambda: threading.Thread(target=popupmsg("Not supported yet!", "")).start())
+
+        menubar.add_cascade(label="模擬結果(Result)", menu=resultMenu)
+
+        recordMenu = tk.Menu(menubar, tearoff=0)
+        recordMenu.add_command(label="Temp", command=lambda: threading.Thread(target=popupmsg("Not supported yet!", "")).start())
+        recordMenu.add_command(label="Temp", command=lambda: threading.Thread(target=popupmsg("Not supported yet!", "")).start())
+
+        menubar.add_cascade(label="模擬歷史(Record)", menu=recordMenu)
+
+        prefMenu = tk.Menu(menubar, tearoff=0)
+        prefMenu.add_command(label="Temp", command=lambda:threading.Thread(target=popupmsg("Not supported yet!", "")).start())
+        prefMenu.add_command(label="Temp", command=lambda: threading.Thread(target=popupmsg("Not supported yet!", "")).start())
+
+        menubar.add_cascade(label="偏好設定(User Preference)", menu=prefMenu)
+
+        aboutMenu = tk.Menu(menubar, tearoff=0)
+        aboutMenu.add_command(label="幫助", command=lambda: threading.Thread(target=popupmsg("Not supported yet!", "")).start())
+        aboutMenu.add_command(label="版權", command=lambda: threading.Thread(target=popupmsg("Not supported yet!", "")).start())
+        aboutMenu.add_command(label="使用合約", command=lambda: threading.Thread(target=popupmsg("Not supported yet!", "")).start())
+        aboutMenu.add_command(label="Run AERMAP testcase", command=lambda: threading.Thread(target=runAERMAP).start())
+        aboutMenu.add_command(label="Run AERMOD testcase", command=lambda: threading.Thread(target=runAERMOD).start())
+        aboutMenu.add_command(label="Run AERMET testcase", command=lambda: threading.Thread(target=runAERMET).start())
+        tk.Tk.config(self, menu=menubar)
+        menubar.add_cascade(label="關於 About", menu=aboutMenu)
 
 
-        self.frames = {}
-
-        for F in (StartPage, PageOne, PageTwo):
-            frame = F(container, self)
-            self.frames[F] = frame
-            frame.grid(row =0, column =0, sticky = "nsew")
-        self.show_frame(StartPage)
-
-    def show_frame(self,cont):
-        frame = self.frames[cont]
-        frame.tkraise()
-
-class StartPage(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text="Start Page", font = LARGE_FONT)
-        label.pack(pady=10, padx=10)
-
-        button1 = ttk.Button(self, text="Visit Page 1", command=lambda: controller.show_frame(PageOne))
-        button1.pack()
-
-class PageOne(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self,parent)
-        label = tk.Label(self, text="Page One", font=LARGE_FONT)
-        label.pack(pady=10, padx=10)
-
-        button1 = ttk.Button(self, text="Page Two", command=lambda: controller.show_frame(PageTwo))
-        button1.pack()
-
-        button = ttk.Button(self, text="Back to Home", command=lambda: controller.show_frame(StartPage))
-        button.pack()
 
 
 
 
-class PageTwo(tk.Frame):
 
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self,parent)
-        label = tk.Label(self, text="Page Two", font=LARGE_FONT)
-        label.pack(pady=10, padx=10)
 
-        button = ttk.Button(self, text="Page One", command=lambda: controller.show_frame(PageOne))
-        button.pack()
 
-        button1 = ttk.Button(self, text="Back to Home", command=lambda: controller.show_frame(StartPage))
-        button1.pack()
+
+
+        self.withdraw()
+        splash = Splash(self)
+
+        time.sleep(3.5)
+
+        ## finished loading so destroy splash
+        splash.destroy()
+
+        ## show window again
+        self.deiconify()
+
+
+    # def show_frame(self,cont):
+    #     frame = self.frames[cont]
+    #     frame.tkraise()
+
+# class StartPage(tk.Frame):
+#
+#     def __init__(self, parent, controller):
+#         tk.Frame.__init__(self, parent)
+#         label = ttk.Label(self, text="Start Page", font = LARGE_FONT)
+#         label.pack(pady=10, padx=10)
+#
+#         button1 = ttk.Button(self, text="Visit Page 1", command=lambda: controller.show_frame(PageOne))
+#         button1.pack()
+#
+# class PageOne(tk.Frame):
+#
+#     def __init__(self, parent, controller):
+#         tk.Frame.__init__(self,parent)
+#         label = tk.Label(self, text="Page One", font=LARGE_FONT)
+#         label.pack(pady=10, padx=10)
+#
+#         button1 = ttk.Button(self, text="Page Two", command=lambda: controller.show_frame(PageTwo))
+#         button1.pack()
+#
+#         button = ttk.Button(self, text="Back to Home", command=lambda: controller.show_frame(StartPage))
+#         button.pack()
+#
+#
+#
+#
+# class PageTwo(tk.Frame):
+#
+#     def __init__(self, parent, controller):
+#         tk.Frame.__init__(self,parent)
+#         label = tk.Label(self, text="Page Two", font=LARGE_FONT)
+#         label.pack(pady=10, padx=10)
+#
+#         button = ttk.Button(self, text="Page One", command=lambda: controller.show_frame(PageOne))
+#         button.pack()
+#
+#         button1 = ttk.Button(self, text="Back to Home", command=lambda: controller.show_frame(StartPage))
+#         button1.pack()
 
 
 
@@ -175,6 +249,6 @@ class PageTwo(tk.Frame):
 
 
 app = EPAgui()
-app.geometry("1280x720")
+app.geometry("1024x768")
 app.mainloop()
 
